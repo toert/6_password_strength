6949e1bbad86073fb366970e6f1a320f1209d62b
toert <pw177@mail.ru>
2016-10-07 12:58:49 +0400

    функция стала чистой

--- password_strength.py
+++ password_strength.py
@@ -7,17 +7,16 @@ def load_password_blacklist(filepath):
     list_of_bad_passwords = re.findall(r'[\w]+', data_bad_passwords)
     return list_of_bad_passwords
 
-def get_password_strength(password):
+def get_password_strength(password, filepath_to_list):
     if len(password) < 6:
         return 1
     else:
-        filepath_to_blacklist = input('Enter filepath to blacklist:')
         password_strength = 1
         if len(password) > 12:  
             password_strength += 2
         if password.lower() != password and password.upper() != password: 
             password_strength += 1
-        if password not in load_password_blacklist(filepath_to_blacklist):
+        if password not in load_password_blacklist(filepath_to_list):
             password_strength += 3
         if re.findall(r'[\d]', password): 
             password_strength += 1
@@ -28,4 +27,5 @@ def get_password_strength(password):
 
 if __name__ == '__main__':
     password = input('Enter your password to get strength: ')
-    print('Your password is ' + str(get_password_strength(password)) + '/10')
+    filepath_to_blacklist = input('Enter filepath to blacklist:')
+    print('Your password is ' + str(get_password_strength(password,filepath_to_blacklist)) + '/10')
